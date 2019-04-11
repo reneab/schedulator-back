fcd6f8d176e4a4b0e261297acd644cb828bf6036
reneab <rene.abd@gmail.com>
2019-04-11 11:39:55 +0800

    set Allow-Origin to environment variable

--- schedver.js
+++ schedver.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONT_URL || 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
